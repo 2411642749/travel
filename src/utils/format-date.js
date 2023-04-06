385b640b20bd6f566c17677619d8639ad09ebb8d
YGB <2411@163.com>
2023-04-06 22:28:42 +0800

    日期选择

--- src/utils/format-date.js
+++ src/utils/format-date.js
@@ -1,5 +1,11 @@
 import dayjs from "dayjs";
 
+// 日期格式化
 export const formatMonthDay = (data) => {
     return dayjs(data).format('MM-DD')
+}
+
+// 两个日期之间的天数的差值
+export const diffDate = (nowDate, nextDate) => {
+    return dayjs(nextDate).diff(nowDate, 'day')
 }
--- src/views/home/components/home-position.vue
+++ src/views/home/components/home-position.vue
@@ -10,27 +10,35 @@
             </div>
         </div>
         <!--    入住    -->
-        <div class="checkin">
+        <div class="checkin" @click="showDate = true">
             <div class="start">
                 <span>入住</span>
                 <div>{{ startDate }}</div>
             </div>
             <div class="total">
-                <span>共一晚</span>
+                <span>共{{ time }}晚</span>
             </div>
             <div class="end">
                 <span>离店</span>
-                <div>{{ endtDate }}</div>
+                <div>{{ endDate }}</div>
             </div>
         </div>
     </div>
+  <!--  日期  -->
+    <van-calendar
+            v-model:show="showDate"
+            color="#ff9645"
+            :show-confirm="false"
+            type="range"
+            @confirm="onConfirm"
+    />
 </template>
 
 <script setup>
 import {ref} from 'vue'
 import {useRouter} from "vue-router";
 import {useCityStore} from "../../../stores/modules/city";
-import {formatMonthDay} from "../../../utils/format-date";
+import {diffDate, formatMonthDay} from "../../../utils/format-date";
 
 const router = useRouter()
 const cityStore = useCityStore()
@@ -53,7 +61,17 @@ const cityName = cityStore.currentData
 const nowData = new Date()
 const startDate = ref(formatMonthDay(nowData))
 const newtData = nowData.setDate(nowData.getDate() + 1)
-const endtDate = ref(formatMonthDay(nextData))
+const endDate = ref(formatMonthDay(newtData))
+
+// 点击入住
+const showDate = ref(false)
+const time = ref(1)
+const onConfirm = (data) => {
+    startDate.value = formatMonthDay(data[0])
+    endDate.value = formatMonthDay(data[1])
+    time.value = diffDate(startDate.value, endDate.value)
+    showDate.value = false
+}
 </script>
 
 <style scoped>
