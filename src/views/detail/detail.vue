ada6314462e0fd740c833e4450b14e13ede21a3b
YGB <2411@163.com>
2023-04-10 22:36:13 +0800

    添加Loading,点击热门跳转

---
+++ src/views/detail/detail.vue
@@ -0,0 +1,28 @@
+<template
+  <div class="detail"> 
+    <van-nav-bar
+      title="旅途"
+      left-text="返回"
+      left-arrow
+      @click-left="onClickLeft"
+    />
+
+    ID:{{ route.params.id }}
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useRouter,useRoute } from 'vue-router';
+
+const router = useRouter()
+const route = useRoute()
+const onClickLeft = () => {
+  router.back()
+
+}
+</script>
+
+<style scoped>
+
+</style>
